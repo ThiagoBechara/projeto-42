a4206dbc2f521d04dc3873b9abb7178079f2c031
ThiagoBechara <95245669+ThiagoBechara@users.noreply.github.com>
2022-08-29 16:12:45 -0300

    Add files via upload

---
+++ drops.js
@@ -0,0 +1,28 @@
+class Drops {
+
+    constructor(x, y) {
+
+        var options = {
+            friction: 0.001,
+            restitution: 0.1
+        }
+        this.rain = Matter.Bodies.circle(x, y, 5, options);
+        this.radius = 5;
+        World.add(world, this.rain);
+    }
+
+update(){
+
+    if(this.rain.position.y > height){
+
+        Matter.Body.setPosition(this.rain, {x:random(0,400), y:random(0,400)})
+    }
+
+}
+display(){
+
+ellipseMode(CENTER);
+ellipse(this.rain.position.x, this.rain.position.y, this.radius, this.radius);
+
+}
+}
